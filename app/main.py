1ac525583982e72ec940110cbc203a3f57e1c4ff
Moss365 <82628786+Moss365@users.noreply.github.com>
2021-05-01 15:21:08 +0800

    add confirm password

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 from app import app
-from flask import render_template, url_for
+from flask import Flask,render_template, url_for,request,flash
+
+app = Flask(__name__)
 
 @app.route("/")
 def index():
@@ -12,3 +14,21 @@ def content():
 @app.route("/login")
 def login():
     return render_template("register.html", title="Register")
+
+@app.route("/templates/register",method=['GET','POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form.get('name')
+        password = request.form.get('pwd')
+        password2 = request.form.get('pwd2')
+        #email , phone
+        if not all([username,password,password2]):
+            flash('Please complete your information')
+        elif password != password2:
+            flash ('The two passwords entered are inconsistent, please enter them again')
+        else:
+            return 'password is correct'
+            
+    return render_template('register.html')
+
+
