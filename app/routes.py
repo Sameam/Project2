c4c1983e070e7f607979f98413a3d8274d5af391
Moss365 <82628786+Moss365@users.noreply.github.com>
2021-05-07 15:39:39 +0800

    add login routes

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from app import app, db
 from flask import render_template, url_for,request,flash,redirect
-from flask_login import current_user, login_user, login_required
+from flask_login import current_user, login_user, login_required, logout_user
 from app.forms import LoginForm, RegistrationForm
 from app.models import User
 from werkzeug.urls import url_parse
@@ -22,8 +22,11 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(name=form.name.data).first()
-        if not user.check_password(form.password.data):
-            flash('Invalid password')
+        if user is None:
+            flash('Your name is wrong, please input correctly name')
+            return redirect(url_for('login'))
+        elif not user.check_password(form.password.data):
+            flash('Your password is wrong, please input correctly password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
@@ -45,4 +48,9 @@ def register():
         flash('Congratulations, you are now a registered user!')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form = form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
 
